48d1adf998cde73fec213860e6fdec410ef7ea68
Lucas Ximenes <lucasdximenes@gmail.com>
2022-08-03 16:54:11 -0300

    Finished styling the Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,10 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { getUser } from '../services/userAPI';
-import Loading from './Loading';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { getUser } from "../services/userAPI";
+import Loading from "./Loading";
+import "../styles/Header.css";
+import logo from "../styles/assets/logo.svg";
+import avatar from "../styles/assets/avatar.svg";
 
 class Header extends Component {
   state = {
@@ -20,21 +23,33 @@ class Header extends Component {
       <header data-testid="header-component">
         {name ? (
           <div className="header-container">
+            <div className="header-logo-avatar-container">
+              <div className="header-logo">
+                <img src={logo} alt="miawtunes" />
+              </div>
+              <div className="avatar-container">
+                <span>{name}</span>
+                <img src={avatar} alt="avatar" />
+              </div>
+            </div>
             <div className="header-nav">
               <nav>
                 <Link to="/search" data-testid="link-to-search">
-                  Search
+                  <button type="button" className="header-nav-btn">
+                    Search
+                  </button>
                 </Link>
                 <Link to="/favorites" data-testid="link-to-favorites">
-                  Favorites
+                  <button type="button" className="header-nav-btn">
+                    Favorites
+                  </button>
                 </Link>
                 <Link to="/profile" data-testid="link-to-profile">
-                  Profile
+                  <button type="button" className="header-nav-btn">
+                    Profile
+                  </button>
                 </Link>
               </nav>
-            </div>
-            <div className="user-header">
-              <span data-testid="header-user-name">{name}</span>
             </div>
           </div>
         ) : (
