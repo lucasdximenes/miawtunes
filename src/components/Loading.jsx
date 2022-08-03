aa232c46ed5662249ad98db95db7799ea97853e1
Lucas Ximenes <lucasdximenes@gmail.com>
2022-08-03 20:54:28 -0300

    fix loading component

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -8,11 +8,13 @@ export default class Loading extends Component {
       <div className="loading">
         <h1>Carregando...</h1>
         <div className="loading-cat-container">
-          <div className="lds-ring">
-            <div />
-            <div />
-            <div />
-            <div />
+          <div className="load-element">
+            <div className="lds-ring">
+              <div />
+              <div />
+              <div />
+              <div />
+            </div>
           </div>
           <img src={loadingCat} alt="loading" />
         </div>
