3144a760a2b63a167e4edeffdaac6bd3085d61b2
Lucas Ximenes <lucasdximenes@gmail.com>
2022-08-03 17:19:46 -0300

    Finished styling the Loading component

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,17 +1,21 @@
-import React, { Component } from 'react';
-import '../styles/Loading.css';
+import React, { Component } from "react";
+import "../styles/Loading.css";
+import loadingCat from "../styles/assets/loading.svg";
 
 export default class Loading extends Component {
   render() {
     return (
       <div className="loading">
-        <div className="lds-ring">
-          <div />
-          <div />
-          <div />
-          <div />
-        </div>
         <h1>Carregando...</h1>
+        <div className="loading-cat-container">
+          <div className="lds-ring">
+            <div />
+            <div />
+            <div />
+            <div />
+          </div>
+          <img src={loadingCat} alt="loading" />
+        </div>
       </div>
     );
   }
